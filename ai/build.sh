f0cb63cc101161f80e196aa6245bd7869d0ec2fe
dvitel <viteldmitry@gmail.com>
2023-11-05 11:32:24 -0500

    deploy to az webapp

--- ai/build.sh
+++ ai/build.sh
@@ -1,9 +1,25 @@
 #!/bin/bash 
-#collectingn dist folder of the flow 
+
+set -e
+
+#collect dist folder of the flow 
 pf flow build --source puzzle --output dist --format docker
+
+#----------------------------------------------------
+#NOTE: next is for local docker build and testing
+
 #build docker img
-docker build dist -t gpt-parsons-gen
+# docker build dist -t gpt-parsons-gen
+
 #use next to run locally 
-# docker run -p 8080:8080 -e PROD-CONN_API_KEY=<key> gpt-parsons-gen
+# docker run -p 8080:8080 -e PROD_CONN_API_KEY=<key> gpt-parsons-gen
+
 #use next to test local endpoint
 # curl -X POST -d '{"language": "Python", "level":"Advanced", "topic":"Recursion", "form":"Python function", "numBugs":1}' http://localhost:8080/score
+
+#----------------------------------------------------
+#NOTE: deploy to Azure App - Azure for Students subs
+# bash deploy.sh --path dist -i gpt-parsons-gen --name gpt-parsons-source -r ebc6ac4765644419945247daba13c00c.azurecr.io -g gpt-parsons -l eastus --subscription d6653226-8c0c-4ac1-b127-b828ee3cf24f 
+
+#testing deployed endpoint
+# curl -X POST -d '{"language": "Python", "level":"Advanced", "topic":"Recursion", "form":"Python function", "numBugs":1}' https://gpt-parsons-source.azurewebsites.net/score
