29105fb066d367afde7498a33c3c97c4c598eaea
ssteinle <91915365+ssteinle@users.noreply.github.com>
2023-12-05 12:32:42 -0500

    updated pipeline

--- stephen_test/.promptflow/gpt_driver.py
+++ stephen_test/.promptflow/gpt_driver.py
@@ -6,6 +6,25 @@ import prompt_library
 # Configure your API key (replace 'your-api-key' with your actual API key)
 api_key = utils.fetch_key()
 
-titles = prompt_library.generate_titles(False)
+topics = 'loops, arrays'
 
-print(titles)
+gpt_model = "gpt-3.5-turbo"
+gpt_model = "gpt-4"
+
+title = prompt_library.generate_title(topics, gpt_model)
+
+psudocode = prompt_library.generate_psudocode(title, topics, gpt_model)
+
+parsons_code = prompt_library.generate_code(title, topics, psudocode, gpt_model)
+
+with open("title.txt", "a") as title_file:
+    for line in title:
+        title_file.write(line + "\n")
+
+with open("pseudocode.txt", "a") as pseudocode_file:
+    for line in psudocode:
+        pseudocode_file.write(line + "\n")
+
+with open("parsons_code.txt", "a") as parsons_file:
+    for line in parsons_code:
+        parsons_file.write(line + "\n")
--- stephen_test/.promptflow/prompt_library.py
+++ stephen_test/.promptflow/prompt_library.py
@@ -1,46 +1,117 @@
 from openai import OpenAI
 
-def generate_titles(sample = True):
+def generate_title(topics = None, gpt_model = "gpt-3.5-turbo"):
     client = OpenAI()
 
-    if sample:
-        user_message = '''
-        Generate a list of five Parsons puzzle titles suitable for high school students learning programming. Each title should reflect a fundamental programming concept or structure in a way that is engaging and educational. The titles should be clear and concise, hinting at the core programming concept being explored in each puzzle. 
+    if topics:
+        user_message = f'''
+        Generate a Parson puzzle title suitable for high school students learning programming. The title should reflect a fundamental programming concept or structure in a way that is engaging and educational. The title should be clear and concise, hinting at the core programming concept being explored in each puzzle. 
 
-        Consider the following programming topics when creating the titles:
-        1. Basic variable types and operations.
-        2. Simple conditional statements (if-else structures).
-        3. Introductory loops (for and while loops).
-        4. Single-dimensional arrays and their basic manipulations.
-        5. The creation and usage of basic functions.
+        Consider the following programming topics when creating the title:
+        {topics}
 
-        Avoid advanced topics and ensure the titles are appealing and understandable to beginners in programming.
+        Avoid advanced topics and ensure the title is appealing and understandable to beginners in programming.
 
-        Example titles:
-        1. "Variable Ventures" - focusing on variable types and operations.
-        2. "If-Else Islands" - based on conditional statements.
-        ...
+        Output only a single title and nothing else:
+
         '''
     else:
         user_message = '''
-        Generate a list of {number_of_titles} Parsons puzzle titles suitable for high school students learning programming. Each title should reflect one of the specified programming concepts or structures in a way that is engaging and educational. The titles should be clear, concise, and appealing to beginners in programming.
+        Generate a Parson puzzle title suitable for high school students learning programming. The title should reflect a fundamental programming concept or structure in a way that is engaging and educational. The title should be clear and concise, hinting at the core programming concept being explored in each puzzle. 
 
-        Programming Topics:
-        {list_of_topics}
+        Consider the following programming topics when creating the title: if-else statements, loops
 
-        For each topic, create a title that hints at the core programming concept being explored in the puzzle. Ensure that the titles are understandable and relevant to the topics provided.
+        Avoid advanced topics and ensure the title is appealing and understandable to beginners in programming.
 
-        Example:
-        If the list of topics includes "Basic variable types and operations" and "Simple conditional statements," then suitable titles might be "Variable Ventures" and "If-Else Islands" respectively.
+        Output only a single title and nothing else:
 
         '''
 
     completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
+    model = gpt_model,
     messages=[
         {"role": "system", "content": "You are teacher and a software engineer designing parsons puzzles for high school students."},
         {"role": "user", "content": user_message}
     ]
     )
 
-    return (completion.choices[0].message)
+    return (completion.choices[0].message.content.split('\n'))
+
+def generate_psudocode(title = None, topics = None, gpt_model = "gpt-3.5-turbo"):
+    client = OpenAI()
+
+    if title and topics:
+        user_message = f'''
+        Create a simple program in psudocode based on the title of "{title}" that clearly embodies the programming concepts of: "{topics}" through its logic and structure.
+        
+        This program will be used for a Parsons puzzle, so it should consist of discrete code blocks that can be rearranged. The program should:
+        - Be beginner-friendly
+        - suitable for high school students
+        - Be divided into distinct code blocks that can be mixed up for the puzzle
+        - Exclude comments to maintain the challenge of the puzzle
+        - Be concise and functionally complete
+        - able to run successfully when arranged correctly
+        
+        Ensure the code is broken down into logical segments that can be rearranged, such as variable declarations, loops, conditional statements, function calls, etc.
+        
+        Comment the code heavily so that it is easily understood. Generate the psudocode block and nothing else:
+
+        '''
+    else:
+        user_message = '''
+        Create a simple program in psudocode based on the title of "If-Else Odyssey" that clearly embodies the programming concepts of: if else statements through its logic and structure.
+        
+        This program will be used for a Parsons puzzle, so it should consist of discrete code blocks that can be rearranged. The program should:
+        - Be beginner-friendly
+        - suitable for high school students
+        - Be divided into distinct code blocks that can be mixed up for the puzzle
+        - Exclude comments to maintain the challenge of the puzzle
+        - Be concise and functionally complete
+        - able to run successfully when arranged correctly
+        
+        Ensure the code is broken down into logical segments that can be rearranged, such as variable declarations, loops, conditional statements, function calls, etc.
+        
+        Comment the code heavily so that it is easily understood. Generate the psudocode block and nothing else:
+
+        '''
+
+    completion = client.chat.completions.create(
+    model = gpt_model,
+    messages=[
+        {"role": "system", "content": "You are teacher and a software engineer designing parsons puzzles for high school students."},
+        {"role": "user", "content": user_message}
+    ]
+    )
+
+    return (completion.choices[0].message.content.split('\n'))
+
+def generate_code(title, topics, pseudocode, gpt_model = "gpt-3.5-turbo"):
+    client = OpenAI()
+
+    if title and topics and pseudocode:
+        user_message = f'''
+        Title: {title}
+
+        Topics: {topics}
+
+        Pseudocode:
+        {pseudocode}
+
+        Based on the provided pseudocode, title, and list of topics, generate a Parsons puzzle that integrates these themes into its structure. The puzzle should not only rearrange code snippets into the correct order but also reflect the overarching themes and concepts indicated in the title and topic list. This will help reinforce the learning objectives for high school students.
+
+        Python Program:
+        [Generate the corresponding Python program that aligns with the pseudocode, title, and topics. Break down the program into discrete, logical blocks that can be used as the basis for the Parsons puzzle. Each block should clearly represent a step or concept in the code, while also being themed around the title and topics provided. The program should be a direct reflection of the pseudocode, ensuring consistency and thematic relevance. Make sure each line has a step comment associated with it for easy reorganization as a parson's puzzle]
+        
+        Comment the code heavily so that it is easily understood. Generate the code block, comments, and nothing else:
+
+        '''
+
+    completion = client.chat.completions.create(
+    model = gpt_model,
+    messages=[
+        {"role": "system", "content": "You are teacher and a software engineer designing parsons puzzles for high school students."},
+        {"role": "user", "content": user_message}
+    ]
+    )
+
+    return (completion.choices[0].message.content.split('\n'))
